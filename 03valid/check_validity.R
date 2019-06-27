b62d171029264a4a011d09716a55ceda02ad1eed
Mark van der Loo <mark.vanderloo@gmail.com>
2019-06-27 13:54:53 +0200

    wip

---
+++ 03valid/check_validity.R
@@ -0,0 +1,57 @@
+
+## Data validation ----
+
+# let's read in the matched data.
+companies <- read.csv("02input/input.csv")
+head(companies)
+
+# try the following code
+
+# load the library
+library(validate)
+
+# define some restrictions on data
+rules <- validator(
+  staff >= 0
+  , turnover >= 0
+  , other.rev >= 0
+  , turnover + other.rev == total.rev
+  , if (staff > 0) staff.costs >= 0
+)
+
+# confront data with the rules
+result <- confront(companies, rules, key="id")
+
+# print the result
+result
+
+# Summarize the result and study the output
+summary(result)
+# Can you explain these results to yourself?
+
+# plot the result
+plot(result)
+# Explain what you see
+
+# add the rule 'mean turnover >= 1'
+rules <- rules + validator(V6 = mean(turnover) > 0)
+rules
+
+# Study the variables staff ... vat. Add *at least* three more rules, then:
+## 1. Summarize and plot the results. Interpret the results.
+
+## 2. Use `as.data.frame` to turn read results into a data frame and
+## View the results.
+
+
+## Assignment (on slide, after presentation)
+
+
+
+
+
+
+
+
+
+
