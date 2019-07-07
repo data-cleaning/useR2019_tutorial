80a6415025c84dd20814cad99645408b1894bb9c
Edwin de Jonge <edwindjonge@gmail.com>
2019-07-07 23:52:38 +0200

    updating stuff

--- 03valid/errorlocalization.R
+++ 03valid/errorlocalization.R
@@ -1,6 +1,27 @@
 library(errorlocate)
 
-# load the rules
+# 1 Toy problem
+# suppose we have the following rule set
+rules <- validator(
+  if (married == TRUE) age >= 16,
+  if (attends == "kindergarten") age <= 6
+)
+
+# and our data to be checked is this:
+my_data <- data.frame( age = 3
+                     , married = TRUE
+                     , attends = "kindergarten"
+                     )
+
+# Discuss with your neighbor how we can alter the values so 
+# they comply to the rules. 
+
+# 1b) Which is your prefered one? Why?
+
+# 1c) # use locate_errors with the rules and my_data to find the error.
+
+
+#2 load the rules for our data set:
 rules <- validator(.file = "./03valid/rules.R")
 
 rules
@@ -8,24 +29,14 @@ rules
 # load the data
 data_with_errors <- read.csv("02input/input.csv")
 
-# use locate_errors to find errononuous variables
-# and store this in variable "errors"
+# use locate_errors to find erronuous variables
+# and store the result in variable "errors"
 
 ...
 
 # make a summary of the errors.
+
 # Which fields are found to contain errors?
 
 ...
 
-
-# we have much confidence in turnover, since these also collected via the 
-# tax register
-weight <- sapply(data_with_errors, function(x) 1)
-# Set the weight of turnover to 10 and supply the weight to 
-# locate_errors 
-
-# are less errors found in turnover?
-
-# replace the erronuous field with NA using replace_errors
-# and store as errors_located.csv
